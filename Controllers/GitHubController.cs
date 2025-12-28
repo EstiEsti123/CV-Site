d0f2222806cceadc64929bdaf66f491e15986f6b
EstiEsti123 <st126389@gmail.com>
2025-12-28 18:11:16 +0200

    Fixed GitHubController.cs

--- Controllers/GitHubController.cs
+++ Controllers/GitHubController.cs
@@ -1,39 +1,4 @@
-// using Microsoft.AspNetCore.Mvc;
-// using CV_SITE.Services;
-// using CV_SITE.Models;
 
-
-// namespace CV_SITE.API
-// {
-//     [ApiController]
-//     [Route("api/[controller]")] // הכתובת תהיה api/github
-//     public class GitHubController : ControllerBase
-//     {
-//         private readonly IGitHubService _gitHubService;
-
-//         // הזרקת השירות (Dependency Injection)
-//         public GitHubController(IGitHubService gitHubService)
-//         {
-//             _gitHubService = gitHubService;
-//         }
-
-//         // 1. שליפת הפורטפוליו שלך
-//         [HttpGet("portfolio/{username}")]
-//         public async Task<ActionResult<List<RepositoryDto>>> GetPortfolio(string username)
-//         {
-//             var repos = await _gitHubService.GetPortfolioAsync(username);
-//             return Ok(repos);
-//         }
-
-//         // 2. חיפוש כללי ב-GitHub
-//         [HttpGet("search")]
-//         public async Task<ActionResult<List<RepositoryDto>>> Search([FromQuery] string? term, [FromQuery] string? language, [FromQuery] string? user)
-//         {
-//             var results = await _gitHubService.SearchRepositoriesAsync(term, language, user);
-//             return Ok(results);
-//         }
-//     }
-// }
 using Microsoft.AspNetCore.Mvc;
 using CV_SITE.Services;
 using CV_SITE.Models;
@@ -52,7 +17,6 @@ namespace CV_SITE.API
             _gitHubService = gitHubService;
         }
 
-        // --- כאן מתחיל החלק שהיה חסר ---
 
         [HttpGet("portfolio/{username}")]
         public async Task<ActionResult<List<RepositoryDto>>> GetPortfolio(string username)
@@ -82,4 +46,4 @@ namespace CV_SITE.API
             }
         }
     }
-} // ודאי שיש כאן סוגר מסולסל סוגר עבור ה-class ועוד אחד עבור ה-namespace
+} 
