7b8f0e1a3278d9441a330eabf22af2a1e5bd2288
EstiEsti123 <st126389@gmail.com>
2025-12-28 18:11:48 +0200

    Fixed RepositoryDTO.cs

--- Models/RepositoryDTO.cs
+++ Models/RepositoryDTO.cs
@@ -1,15 +1,4 @@
-// namespace CV_SITE.Models // החליפי את YourProjectName בשם הפרויקט שלך
-// {
-//     public class RepositoryDto
-//     {
-//         public string Name { get; set; }           // שם הפרויקט
-//         public string Language { get; set; }       // שפת פיתוח עיקרית
-//         public int StarsCount { get; set; }        // כמות כוכבים
-//         public DateTimeOffset? LastCommit { get; set; } // תאריך עדכון אחרון
-//         public string RepoUrl { get; set; }        // קישור לגיטהאב
-//         public string Homepage { get; set; }       // קישור לאתר הפרויקט (אם יש)
-//     }
-// }
+
 namespace CV_SITE.Models
 {
     public class RepositoryDto
